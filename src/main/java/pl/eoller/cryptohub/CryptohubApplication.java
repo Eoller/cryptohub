a17a7725d1a7aa4f9a159bc2514d2ca4432643e6
eoller <Egorpyp@gmail.com>
2018-12-06 23:37:01 +0100

    init commit

---
+++ src/main/java/pl/eoller/cryptohub/CryptohubApplication.java
@@ -0,0 +1,12 @@
+package pl.eoller.cryptohub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptohubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CryptohubApplication.class, args);
+    }
+}
